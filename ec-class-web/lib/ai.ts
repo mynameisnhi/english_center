559101f3de597c5834bcfddc0d42dfee174ba5d8
mynameisnhi <tuyetnhi260802@gmail.com>
2024-12-23 20:27:11 +0700

    update promt

--- ec-class-web/lib/ai.ts
+++ ec-class-web/lib/ai.ts
@@ -35,10 +35,15 @@ const detectContent = (content: string, courses: Courses[]) => {
   const regex = [/tu van/, /khoa hoc/, /lo trinh/, /hoc phi/];
   const text = removeAccents(content.toLowerCase());
   if (regex.some((r) => r.test(text))) {
-    return `Bạn là chuyên gia tư vấn cho học viên, trung tâm có các khóa học và học phí như sau:
+    return `Bạn là chuyên gia tư vấn cho học viên tại Trung tâm tiếng Anh. Trung tâm có các khóa học và học phí như sau:
     ${courses
       .map((c) => `Khóa học ${c.name} với học phí ${c.price}$`)
-      .join(', ')}. Bạn hãy tư vấn cho học viên với những câu hỏi của họ.`;
+      .join(
+        ', '
+      )}. Khi tư vấn cho học viên, bạn chỉ nên trả lời các câu hỏi liên quan đến các khóa học và thông tin học phí được cung cấp ở trên.
+      Nếu khóa học mà học viên hỏi không có hoặc không phù hợp thì đừng tiếp tục.
+      Ngoài ra, bạn không được trả lời các câu hỏi về những chủ đề không liên quan đến khóa học hoặc thông tin bên ngoài ứng dụng này.
+      Hãy luôn tập trung vào việc cung cấp thông tin chính xác và hữu ích liên quan đến các khóa học và học phí của trung tâm.`;
   }
   return '';
 };
